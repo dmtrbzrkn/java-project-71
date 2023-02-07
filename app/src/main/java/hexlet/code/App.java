2303720d1cd62e586bdfe3f742af1d03acb3334f
Bazarkin Dmitrii <bazarkin.dmitrii@gmail.com>
2023-02-07 19:35:43 +0700

    step four

---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,34 @@
+package hexlet.code;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.util.concurrent.Callable;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
+        description = "Compares two configuration files and shows a difference.")
+public class App implements Callable<Integer> {
+    private static final int EXIT_CODE = 0;
+
+    @Option(names = {"-f", "--format"}, defaultValue = "stylish", paramLabel = "format",
+            description = "output format [default: ${DEFAULT-VALUE}]")
+    private String formatName;
+    @Parameters(index = "0", paramLabel = "filepath1",
+            description = "path to first file")
+    private String filePath1;
+    @Parameters(index = "1", paramLabel = "filepath2",
+            description = "path to second file")
+    private String filePath2;
+
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
+
+    @Override
+    public Integer call() {
+        return EXIT_CODE;
+    }
+}

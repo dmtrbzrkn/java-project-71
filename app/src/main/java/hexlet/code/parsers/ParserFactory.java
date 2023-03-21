93f39d196daae1f1d5d7548b746a3561b662171c
Bazarkin Dmitrii <bazarkin.dmitrii@gmail.com>
2023-03-21 22:45:30 +0700

    check3

--- app/src/main/java/hexlet/code/parsers/ParserFactory.java
+++ app/src/main/java/hexlet/code/parsers/ParserFactory.java
@@ -3,11 +3,11 @@ package hexlet.code.parsers;
 
 public class ParserFactory {
 
-    public static Parser getParser(String dataFormat) throws Exception {
+    public static Parser getParser(String dataFormat) {
         return switch (dataFormat.toUpperCase()) {
             case "JSON" -> new JSONParser();
             case "YML", "YAML" -> new YMLParser();
-            default -> throw new IllegalArgumentException("Wrong data format:" + dataFormat);
+            default -> throw new Error("No such format" + dataFormat);
         };
     }
 }
